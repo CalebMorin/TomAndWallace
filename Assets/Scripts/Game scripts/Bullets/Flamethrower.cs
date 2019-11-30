0579446f59f9c5ac5e104d15430d58ecee85a9c4
Caleb Morin <cmorin2@my.centennialcollege.ca>
2019-11-30 11:08:05 -0500

    beta release 2

---
+++ Assets/Scripts/Game scripts/Bullets/Flamethrower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    public GameObject explosion;
+    public int damage;
+    private GameController gameController;
+
+    [Header("Sounds")]
+    public AudioSource flam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player2")
+        {
+            flam.Play();
+            gameController.player2HP -= damage;
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Wall")
+        {
+            flam.Play();
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game scripts/Bullets/Flamethrower2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower2 : MonoBehaviour
+{
+    public int damage;
+    public GameObject explosion;
+    private GameController gameController;
+
+    [Header("Sounds")]
+    public AudioSource flam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            flam.Play();
+            gameController.player1HP -= damage;
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Wall")
+        {
+            flam.Play();
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
